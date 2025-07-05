c6ad7ab68fd8997b7c15f35334197a2550475207
Nasir Idrishi <nasiridrishi@outlook.com>
2025-07-05 10:33:41 +0530

    Remove archive directory from version control and update .gitignore

--- archive/prototype-phase/focushive-prototype/server/src/sockets/forumSocket.ts
+++
@@ -1,8 +0,0 @@
-import { Server } from 'socket.io';
-import { setupForumHandlers } from './forumHandlers';
-
-export function setupForumSockets(io: Server) {
-  io.on('connection', (socket) => {
-    setupForumHandlers(io, socket);
-  });
-}
--- archive/prototype-phase/focushive-prototype/shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all types
-export * from './types';
-
-// Export all constants
-export * from './constants';
